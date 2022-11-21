a20e0c9df3c32083d921ea36314681039ac6d968
tusharhow <tusharsoftdev@gmail.com>
2022-11-20 22:40:51 -0800

    Name change

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DogDom',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
